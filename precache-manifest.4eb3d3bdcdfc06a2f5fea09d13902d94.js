d8ce47acaabd64c9ed13256379e5394ff0bf7c68
Nazar32 <Nazar32@users.noreply.github.com>
2020-10-17 17:52:25 +0000

    deploy: 8d9170b4c2c408ae4d3896348b8db20a4e2ddd6e

---
+++ precache-manifest.4eb3d3bdcdfc06a2f5fea09d13902d94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73826cdbfb0a254bec672b2b927ebaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7770652852d6deda77",
+    "url": "/static/css/2.455cd81f.chunk.css"
+  },
+  {
+    "revision": "66171e74c1c97b434810",
+    "url": "/static/css/main.bba2c004.chunk.css"
+  },
+  {
+    "revision": "fe7770652852d6deda77",
+    "url": "/static/js/2.2da71069.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2da71069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66171e74c1c97b434810",
+    "url": "/static/js/main.f39f1710.chunk.js"
+  },
+  {
+    "revision": "53c872d770f1ea5da78a",
+    "url": "/static/js/runtime-main.a17df252.js"
+  }
+]);
